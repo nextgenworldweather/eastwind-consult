b664d7c82083a008bf7ad8afc6d05118194b5b89
Peter Nunekpeku <nextgen.worldweather@gmail.com>
2024-11-22 17:26:04 +0000

    Update

--- src/index.jsx
+++ src/index.jsx
@@ -11,18 +11,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
-/*
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-*/
