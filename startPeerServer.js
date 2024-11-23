4e9b21b453b968ae29a3ee28f6561563fbacce99
Peter Nunekpeku <nextgen.worldweather@gmail.com>
2024-11-23 15:33:42 +0000

    Update

--- startPeerServer.js
+++
@@ -1,8 +0,0 @@
-const { PeerServer } = require('peer');
-
-const peerServer = PeerServer({
-  port: 443,
-  path: '/services/peerjs',
-});
-
-console.log('PeerServer listening on port 443');
---
+++ startPeerServer.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import Peer from 'peerjs';
+
+function startPeerServer() {
+  const [peer, setPeer] = useState(null);
+
+  useEffect(() => {
+    const peer = new Peer(undefined, {
+      host: '/',
+      port: 9000,
+      path: '/services/peerjs'
+    });
+
+    setPeer(peer);
+
+    peer.on('open', (id) => {
+      console.log('My peer ID is ' + id);
+    });
+
+    return () => {
+      peer.destroy();
+    };
+  }, []);
+
+  return (
+    <div>
+      {/* ... */}
+    </div>
+  );
+}
+
+export default startPeerServer;
