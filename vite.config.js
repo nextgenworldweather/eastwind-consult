14ac58c36fabf967706ff2475a022a2586e6e952
Peter Nunekpeku <nextgen.worldweather@gmail.com>
2024-11-24 16:30:32 +0000

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import commonjs from 'vite-plugin-commonjs';
 import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [react(), commonjs()],
+  plugins: [react()],
   base: '/eastwind-consult/', // Required for GitHub Pages
   build: {
     outDir: 'dist',
@@ -19,7 +18,6 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      'core-js-pure': 'core-js-pure/es',
       '@': resolve(__dirname, './src'),
     },
     extensions: ['.js', '.jsx', '.json'],
